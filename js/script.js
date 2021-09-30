9c21afc488f04a16f4117a0a66d58ff56b6b097d
jskimweb <jskimweb@gmail.com>
2021-10-01 01:49:23 +0900

    Complete project to responsive

--- js/script.js
+++ js/script.js
@@ -1,20 +1,70 @@
 window.onload = function () {
+	// 스크롤 시 Header 고정
 	const header = $('.header');
 	const headerScr = 'header--scroll';
 
-	// visual slide
-	new Swiper('.sw-visual', {
+	$(window).scroll(function () {
+		let winScrTop = $(window).scrollTop();
+
+		if (winScrTop > 0) {
+			header.addClass(headerScr);
+		} else {
+			header.removeClass(headerScr);
+		}
+	});
+
+	// 모바일 gnb 보이기/숨기기
+	const mGnbBtn = $('.m-gnb-btn');
+	const mGnb = $('.m-gnb');
+	const mGnbClose = $('.m-gnb__close');
+	const blackBg = $('.black-bg');
+
+	mGnbBtn.click(function () {
+		mGnb.show();
+		blackBg.show();
+		$('html, body').css('overflow', 'hidden');
+	});
+	mGnbClose.click(function () {
+		mGnb.hide();
+		blackBg.hide();
+		$('html, body').removeAttr('style');
+	});
+
+	// 리사이징된 윈도우 크기에 따른 모바일 gnb 자동 숨기기
+	$(window).resize(function () {
+		if ($(window).outerWidth() > 1060) {
+			mGnb.hide();
+			blackBg.hide();
+			$('html, body').removeAttr('style');
+		}
+	});
+
+	// 모바일 gnb 서브메뉴 보이기/숨기기
+	const mMainmenus = [$('.m-mainmenu01'), $('.m-mainmenu02'), $('.m-mainmenu03')];
+	const mDep02s = [$('.m-mainmenu01+.m-dep02'), $('.m-mainmenu02+.m-dep02'), $('.m-mainmenu03+.m-dep02')];
+
+	$.each(mMainmenus, function (index) {
+		$(this).click(function () {
+			$(this).toggleClass('m-mainmenu--active');
+			mDep02s[index].toggleClass('m-dep02--show');
+		})
+	});
+
+	// visual 슬라이드
+	let sw_visual = new Swiper('.sw-visual', {
 		loop: true,
 		speed: 1000,
 		autoplay: {
 			delay: 5000,
+			disableOnInteraction: false,
 		},
 		pagination: {
 			el: '.sw-visual__pg',
+			clickable: true,
 		},
 	});
 
-	// go-top
+	// Go-top
 	const goTop = $('.go-top');
 	const goTopShow = 'go-top--show';
 
@@ -24,22 +74,23 @@ window.onload = function () {
 		});
 	});
 
-	// scroll
+	// 스크롤 위치에 따른 Go-top 보이기/숨기기
 	$(window).scroll(function () {
 		let winScrTop = $(window).scrollTop();
 
-		// header fix
-		if (winScrTop > 0) {
-			header.addClass(headerScr);
-		} else {
-			header.removeClass(headerScr);
-		}
-
-		// go-top show & hide
 		if (winScrTop >= 450) {
 			goTop.addClass(goTopShow);
 		} else {
 			goTop.removeClass(goTopShow);
 		}
+	});
+
+	// footer-info 보이기/숨기기
+	const footerBtn = $('.footer__btn');
+	const footerInfo = $('.footer-info');
+	const footerInfoShow = 'footer-info--show';
+
+	footerBtn.click(function () {
+		footerInfo.toggleClass(footerInfoShow);
 	});
 }
